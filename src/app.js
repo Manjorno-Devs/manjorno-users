6a92931da2e4f1560ba211f4f833cd4af9889490
thermalthrottle <biz.mitko@gmail.com>
2021-08-30 20:28:53 +0300

    pipeline test

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
-const express = require("express");
+//modules
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
+const env = dotenv.config();
 const app = express();
 
+
+app.use (bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors());
+
 app.get('/', (req, res) => {
-    res.status(418).json({title:"Teapot"});
-})
+    res.status(418).json({"title":"Az sum kafe"});
+});
 
-console.log("Listening...");
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser:true, useUnifiedTopology: true})
+    .then(result => {
+        var port = process.env.PORT || 3100;
+        console.log(`Connected to DB. Server listening on ${port}`);
+        app.listen(port);
+    })
+    .catch(error => {
+        console.log("Can't connect to database, because of this error: ");
+        console.log(error);
+    });
 
-app.listen(process.env.PORT || 3100);
+export default app;
