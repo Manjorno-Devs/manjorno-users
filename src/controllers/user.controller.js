994c0294f43a4dd1b6b6f7dd4769a48ad1c166f2
thermalthrottle <biz.mitko@gmail.com>
2021-09-06 01:57:45 +0300

    Added error for incorect data

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,21 +1,27 @@
 import UserService from '../services/user.service.js';
 
 export const RegisterUser = async (req, res) => {
-    const {username, password, repeatedPassword, email, profilePicture} = req.body;
-    if (password != repeatedPassword) {
-        res.status(400).json({"message": "Passwords do not match!"});
-        return;
+    
+    try {
+        const {username, password, repeatedPassword, email, profilePicture} = req.body;
+        if (password != repeatedPassword) {
+            res.status(400).json({"message": "Passwords do not match!"});
+            return;
+        }
+        const userByUsername = await UserService.FindUserByUsername(username);
+        if (userByUsername) {
+            res.status(400).json({"message": "Username is already taken!"});
+            return;
+        }
+        const userByEmail = await UserService.FindUserByEmail(email);
+        if (userByEmail) {
+            res.status(400).json({"message":"User with such e-mail is already registered!"});
+            return;
+        }
+        await UserService.RegisterUser(username, password, email);
+        res.status(200).json({"message": "User registered successfully!"});
+    } catch (error) {
+        res.status(500).json({"title": "It's not you, it's us!", "text":`Here's a cookie in the mean time while the bug is hopefully getting fixed`, "errorMessage": `${error}`});
     }
-    const userByUsername = await UserService.FindUserByUsername(username);
-    if (userByUsername) {
-        res.status(400).json({"message": "Username is already taken!"});
-        return;
-    }
-    const userByEmail = await UserService.FindUserByEmail(email);
-    if (userByEmail) {
-        res.status(400).json({"message":"User with such e-mail is already registered!"});
-        return;
-    }
-    UserService.RegisterUser(username, password, email);
-    res.status(200).json({"message": "User registered successfully!"});
+    
 }
