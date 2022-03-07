aa906453d9b29d10ff36d28acd70a6d4b85cf0d5
thermalthrottle <biz.mitko@gmail.com>
2022-03-08 00:52:44 +0200

    client secret fix

--- src/helpers/AuthenticateKeycloak.js
+++ src/helpers/AuthenticateKeycloak.js
@@ -1,5 +1,7 @@
 const Authenticate = async (KeycloakAdminClient) => {
     try {
+        console.log(process.env.KEYCLOAK_USERNAME);
+        console.log(process.env.KEYCLOAK_PASSWORD);
         console.log(process.env.KEYCLOAK_CLIENTCLI_SECRET);
         return await KeycloakAdminClient.auth({
             username: process.env.KEYCLOAK_USERNAME,
